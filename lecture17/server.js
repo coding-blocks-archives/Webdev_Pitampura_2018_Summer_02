c591977cb8b147fb2eaa1174f911b84f093fbca0
Rishabh Khanna <rishabhkhanna777@gmail.com>
2018-07-20 16:40:54 +0530

    handle bars todolist
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- lecture17/server.js
+++ lecture17/server.js
@@ -4,12 +4,24 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.set("view engine", "hbs");
 // app.set("views", "pathToFolder");
-
+var todos = [
+    {task: "first task"}
+];
 app.get('/', (req,res)=>{
-        res.render("index", {body: "Hello coding blocks"})
-    // res.send("hello world")
+        res.render("index", {
+            todos:todos
+        })
+
+})
+
+app.post('/', (req,res)=>{
+    todos.push({task: req.body.task});
+    res.redirect("/");
 })
 
 app.listen(9090, ()=>{
